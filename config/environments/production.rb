d29325e6597d0bd9aa96d7d2072d28e86b58f8f9
Shogo <vivivaund178@icloud.com>
2024-11-26 21:24:19 +0900

    add config/environments/production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,4 +90,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # Allow host to be accessed from the outside
+  config.hosts << "filmusic.onrender.com"
 end
