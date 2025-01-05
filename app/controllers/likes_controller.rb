b1dcfe7b6dadc6052adf2dd58cca4b4e5b470d1d
Shogo <vivivaund178@icloud.com>
2025-01-05 15:21:00 +0900

    fix like feature

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,31 +2,33 @@ class LikesController < ApplicationController
   before_action :set_post
 
   def create
-    @like = @post.likes.create(user: current_user)
+    @like = @post.likes.new(user: current_user)
 
-    respond_to do |format|
-      format.turbo_stream  # Turbo Streamで部分的な更新を処理
-      format.html { redirect_to posts_path }
+    if @like.save
+      respond_to do |format|
+        format.turbo_stream
+        format.html { redirect_to @post, notice: 'いいねしました' }
+      end
+    else
+      # エラーハンドリング
     end
   end
 
   def destroy
     @like = @post.likes.find_by(user: current_user)
-    @like.destroy if @like
-
-    respond_to do |format|
-      format.turbo_stream  # Turbo Streamで部分的な更新を処理
-      format.html { redirect_to posts_path }
+    if @like&.destroy
+      respond_to do |format|
+        format.turbo_stream
+        format.html { redirect_to @post, notice: 'いいねを取り消しました' }
+      end
+    else
+      # エラーハンドリング
     end
   end
 
   private
 
   def set_post
-    @post = Post.find_by(id: params[:post_id])
-    if @post.nil?
-      flash[:alert] = '投稿が見つかりません'
-      redirect_to posts_path
-    end
+    @post = Post.find(params[:post_id])
   end
 end
