2cf75fe0bde6ab8cee8ab25b5cea7eceaac8d96c
Shogo <vivivaund178@icloud.com>
2025-01-07 15:47:40 +0900

    fix like feature

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,30 +1,35 @@
 class LikesController < ApplicationController
   before_action :set_post
 
+  # LikesController
   def create
-    @like = @post.likes.new(user: current_user)
-
-    if @like.save
-      respond_to do |format|
-        format.turbo_stream
-        format.html { redirect_to @post, notice: 'いいねしました' }
+    @like = @post.likes.find_or_create_by(user: current_user)
+  
+    respond_to do |format|
+      format.turbo_stream do
+        render turbo_stream: turbo_stream.replace("like-button-#{@post.id}", partial: "posts/like_button", locals: { post: @post })
       end
-    else
-      # エラーハンドリング
+      format.html { redirect_to @post, notice: 'いいねしました' }
     end
-  end
+  end  
 
   def destroy
     @like = @post.likes.find_by(user: current_user)
+  
     if @like&.destroy
       respond_to do |format|
-        format.turbo_stream
+        format.turbo_stream do
+          render turbo_stream: turbo_stream.replace("like-button-#{@post.id}", partial: "posts/like_button", locals: { post: @post })
+        end
         format.html { redirect_to @post, notice: 'いいねを取り消しました' }
       end
     else
-      # エラーハンドリング
+      # エラーハンドリング（例: いいねが存在しない場合）
+      respond_to do |format|
+        format.html { redirect_to @post, alert: 'エラーが発生しました' }
+      end
     end
-  end
+  end  
 
   private
 
