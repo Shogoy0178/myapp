f682160e6c432e9e3f382e776686a815e6d2a620
Shogo <vivivaund178@icloud.com>
2025-01-07 16:48:01 +0900

    fix bug

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,7 @@ class UsersController < ApplicationController
 
     if @user.save
       flash[:notice] = "ユーザー登録が成功しました！"
-      redirect_to root_path
+      redirect_to login_path
     else
       flash[:alert] = "ユーザー登録に失敗しました。"
       flash[:error_messages] = @user.errors.full_messages # エラーメッセージをflashに格納
@@ -29,7 +29,6 @@ class UsersController < ApplicationController
     redirect_to root_path, alert: "アクセス権がありません。" unless @user == current_user
   end
   
-
   private
 
   def user_params
